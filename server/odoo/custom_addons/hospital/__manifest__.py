e73f77be8897de79aaf7b1de8e41f1672f641e49
Sijan Shrestha <coolsijan12@gmail.com>
2024-08-07 16:37:39 +0800

    model created

---
+++ server/odoo/custom_addons/hospital/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'Hospital Solution',
+    'version': '1.0.0',
+    'sequence': -100,
+    'author': 'Sijan Shrestha',
+    'category': 'Hospital',
+    'summary': 'Hospital Management System',
+    'description': """
+This module contains all the  features of Hospital Management.
+    """,
+    'depends': [],
+    'data': [],
+    'demo': [],
+    # 'installable': True,
+    'auto_install': False,
+    'application': True,
+    'license': 'LGPL-3',
+}
