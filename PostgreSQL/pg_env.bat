f59e93b71b7b2755711348a2615ecc06aa9b3820
Sijan Shrestha <coolsijan12@gmail.com>
2024-08-07 15:42:28 +0800

    models created

---
+++ PostgreSQL/pg_env.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+REM The script sets environment variables helpful for PostgreSQL
+
+@SET PATH="D:\Odoo15\PostgreSQL\bin";%PATH%
+@SET PGDATA=D:\Odoo15\PostgreSQL\data
+@SET PGDATABASE=postgres
+@SET PGUSER=odoo15
+@SET PGPORT=5432
+@SET PGLOCALEDIR=D:\Odoo15\PostgreSQL\share\locale
+
+                            
